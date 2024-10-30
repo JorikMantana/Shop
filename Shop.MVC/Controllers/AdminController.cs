a256ab98ca4141dd5e5782d81a06b5d3b28f76fc
Jorik Mantana <bicksonboy@gmail.com>
2024-10-30 13:51:03 +0400

    frontend: comments, createPanel

---
+++ Shop.MVC/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult CreateNewProduct()
+        {
+            return View();
+        }
+    }
+}
